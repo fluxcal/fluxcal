53a508459421a093abb3ce8d267b0c557a510bee
Timon F <dev@timonf.de>
2014-11-27 00:52:08 +0100

    added very simple event handling, and implemented attribute writer

--- src/FluxCal/Writer/IcsWriter.php
+++ src/FluxCal/Writer/IcsWriter.php
@@ -3,6 +3,8 @@
 namespace FluxCal\Writer;
 
 use FluxCal\Model\CalendarInterface;
+use FluxCal\Model\Event;
+use FluxCal\Model\EventInterface;
 
 /**
  * Class IcsWriter
@@ -11,9 +13,19 @@ use FluxCal\Model\CalendarInterface;
  */
 class IcsWriter implements WriterInterface, CalendarAwareInterface
 {
-    const NEW_LINE = 0x0D0A; // CR+LF, by RFC 5545 3.1
+    const NEW_LINE = "\r\n"; // CR+LF, by RFC 5545 3.1
 
     const LINE_LENGTH = 75;
+
+    const VERSION = '2.0';
+
+    const TYPE_CALENDAR = 'VCALENDAR';
+    const TYPE_EVENT = 'VCALENDAR';
+
+    /**
+     * @var CalendarInterface
+     */
+    protected $calendar;
 
     /**
      * Returns a single VCALENDAR attribute.
@@ -26,15 +38,92 @@ class IcsWriter implements WriterInterface, CalendarAwareInterface
      */
     public function writeAttribute($name, $value)
     {
-        return '';
+        $attribute = strtoupper($name) . ':' . $value;
+        $result    = '';
+        $indent    = 0;
+
+        while(substr($attribute, (self::LINE_LENGTH + $indent) * -1) != '') {
+            if ($indent == 1) {
+                $attribute = ' ' . $attribute;
+            }
+
+            $result   .= substr($attribute, 0, (self::LINE_LENGTH));
+            $attribute = substr($attribute, (self::LINE_LENGTH));
+
+            $indent = 1;
+            $result .= self::NEW_LINE;
+        }
+
+        return $result;
     }
+
+    /**
+     * Opens a section of a component (something like BEGIN:VEVENT)
+     *
+     * @param string $component
+     *
+     * @return string
+     * @author Timon F <dev@timonf.de>
+     */
+    public function writeOpenSection($component)
+    {
+        return $this->writeAttribute('begin', strtoupper($component));
+    }
+
+    /**
+     * Closes a section of a component (something like BEGIN:VEVENT)
+     *
+     * @param string $component
+     *
+     * @return string
+     * @author Timon F <dev@timonf.de>
+     */
+    public function writeCloseSection($component)
+    {
+        return $this->writeAttribute('end', strtoupper($component));
+    }
+
 
     /**
      * {@inheritdoc}
      */
     public function setCalendar(CalendarInterface $calendar)
     {
+        $this->calendar = $calendar;
+    }
 
+    /**
+     * Writes events (@todo: don't forget other types like journal)
+     *
+     * @return string
+     * @author Timon F <dev@timonf.de>
+     */
+    public function writeEvents()
+    {
+        $iCal = '';
+        foreach($this->calendar->getEvents() as $event) {
+            $iCal .= $this->writeEvent($event);
+        }
+        return $iCal;
+    }
+
+    /**
+     * Writes a single event.
+     *
+     * @param EventInterface $event
+     *
+     * @return string
+     * @author Timon F <dev@timonf.de>
+     */
+    public function writeEvent(EventInterface $event)
+    {
+        $iCal = $this->writeOpenSection(self::TYPE_EVENT);
+        if ($event instanceof Event) {
+            $iCal .= $this->writeAttribute('description', $event->getDescription());
+            // todo: date time conversion (\DateTime to ics/rfc standard)
+        }
+        $iCal .= $this->writeCloseSection(self::TYPE_EVENT);
+        return $iCal;
     }
 
     /**
@@ -42,6 +131,11 @@ class IcsWriter implements WriterInterface, CalendarAwareInterface
      */
     public function write()
     {
-        return '';
+        $iCal = $this->writeOpenSection(static::TYPE_CALENDAR);
+        $iCal .= $this->writeAttribute('version', static::VERSION);
+        $iCal .= $this->writeEvents();
+        $iCal .= $this->writeCloseSection(static::TYPE_CALENDAR);
+
+        return $iCal;
     }
 }
--- tests/FluxCal/Writer/IcsWriterTest.php
+++ tests/FluxCal/Writer/IcsWriterTest.php
@@ -27,6 +27,18 @@ class IcsWriterTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('bar', $line);
     }
 
+    public function testWriteAttributeUsingLongValues()
+    {
+        $longText = 'First' . str_pad('', 160, 'Test ') . ' Last';
+        $line = $this->icsWriter->writeAttribute('foo', $longText);
+        $lines = explode(IcsWriter::NEW_LINE, $line);
+
+        $this->assertCount(4, $lines); // 3 (from 160/75), and one at the end.
+        $this->assertContains('Test', $line);
+        $this->assertContains('First', $line);
+        $this->assertContains('Last', $line);
+    }
+
     public function testWriteIsNotEmpty()
     {
         $output = $this->icsWriter->write();
